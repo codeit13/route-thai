1ba655cd746113e1022dfbf341920c6e38d88a5b
kaushik <kaushik.moment@outlook.com>
2021-05-13 10:56:28 +0530

    User dashboard

---
+++ app/resources/views/front/user/_sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="col-lg-2 col-xs-12 flush">
+    <div class="sidebar">
+       <ul>
+          <li class="active"><a href="#"><i class="fal fa-tv-alt"></i> Dashboard</a>
+          </li>
+          <li> <a href="#"><i class="fal fa-user"></i> Basic Info</a>
+          </li>
+          <li> <a href="#"><i class="fal fa-credit-card"></i> Payment</a>
+          </li>
+          <li> <a href="#"><i class="fal fa-lock"></i> Securtiy</a>
+          </li>
+       </ul>
+    </div>
+ </div>
