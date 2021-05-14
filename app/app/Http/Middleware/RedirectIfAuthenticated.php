288a55ee8b2ff3d390b05744c62d232cd5d4b015
kaushik <kaushik.moment@outlook.com>
2021-05-14 13:00:34 +0530

    Bug Fixes

--- app/app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,15 +18,16 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            
-            if($guard == 'admin'){
+        
+        if($guard == 'admin'){
+            if (Auth::guard($guard)->check()) {
                 return $next($request);    
             }
             else 
             {
                 return redirect('admin/login');
             }
+        }
             return redirect(RouteServiceProvider::HOME);
         }
 // dd($guard);
