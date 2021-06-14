b7846ad19337574a1c02276d519ff25121f1252a
Niraj <nmsavaliya94@hotmail.com>
2021-06-14 13:30:06 +0530

    done

--- app/resources/views/front/sell/chat_body.blade.php
+++ app/resources/views/front/sell/chat_body.blade.php
@@ -1,11 +1,7 @@
 <div class="row">
 	@foreach($messages as $single_message)
 		<div class="col-md-12">
-			@if($single_message->from_user == $current_user)
-				<p style="margin-top:1px">You : {{ $single_message->content }}</p>
-			@else
-				<p style="margin-top:1px">{{ $single_message->toUser->name }}: {{ $single_message->content }}</p>
-			@endif
+			<p style="margin-top:1px">{{ $single_message->toUser->name }}: {{ $single_message->content }}</p>
 		</div>
 	@endforeach
 </div>
