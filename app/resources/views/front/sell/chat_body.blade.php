0085a507bf7f394ffdaad89e780f617a1986da25
Niraj <nmsavaliya94@hotmail.com>
2021-06-14 13:43:53 +0530

    done

--- app/resources/views/front/sell/chat_body.blade.php
+++ app/resources/views/front/sell/chat_body.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	@foreach($messages as $single_message)
 		<div class="col-md-12">
-			<p style="margin-top:1px">{{ $single_message->toUser->name }}: {{ $single_message->content }}</p>
+			<p style="margin-top:1px">{{ $single_message->from_user->name }}: {{ $single_message->content }}</p>
 		</div>
 	@endforeach
 </div>
