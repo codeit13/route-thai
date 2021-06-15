3284cc091c9f86851dd5b924bafdb93ed9effede
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-06-15 09:56:53 +0530

    Added Line NOtifications

--- app/resources/views/front/line-bot.blade.php
+++ app/resources/views/front/line-bot.blade.php
@@ -17,7 +17,7 @@
          <div class="col text-center xs-left">
             <h2>Line Bot</h2>
             <p>Add our Line Bot as a friend and get every updates regarding your account right in your line app.</p>
-            <a target="_blank" href="http://line.me/ti/p/@{{ ENV('LINE_BOT_USERID') }}" class="btn btn-success">Add Friend</a>
+            <a target="_blank" href="http://line.me/ti/p/@{{ env('LINE_BOT_USERID') }}" class="btn btn-success">Add Friend</a>
             <br><br>
             <a href="{{ route('user.dashboard') }}" class="btn btn-sm go-back">Go back to Dashboard</a>
          </div>
