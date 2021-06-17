66b6afb0843c573b120ba14472fe21a5bf8d7105
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-06-17 17:40:18 +0530

    Some changes

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -102,10 +102,9 @@ class LoginController extends Controller
 
     private function sendTelegramNotification($user){
         $location =  Authentication_log::whereNotNull('continent_code')->where('authenticatable_id',$user->id)->orderBy('id','DESC')->first();
-        // $welcomeMessage = "Hi ".$user->name . " You currently logged in!";
-        // $welcomeMessage .= "Location: ".$location->city." ".$location->region_name." ".$location->country_name."\n";
-        // $welcomeMessage .= "IP Address: ".$location->ip_address;
-        $welcomeMessage = "You are logged in";
+        $welcomeMessage = 'Hi, ' . $user->name . ', You currently logged in! \n';
+        $welcomeMessage .= 'Location: ' . $location->city. ', ' . $location->region_name . ', ' . $location->country_name . '\n';
+        $welcomeMessage .= 'IP Address: ' . $location->ip_address;
         Log::debug((array) $welcomeMessage);
         Log::debug((array) $user->telegram_user_id);
         $user->notify(new LaravelTelegramNotification([
