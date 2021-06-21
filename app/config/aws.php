29c9074565c17ff6d45d3f0433782e6f099f886c
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-06-21 21:42:03 +0530

    d

--- app/config/aws.php
+++ app/config/aws.php
@@ -20,7 +20,7 @@ return [
         'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
     ],
-    'region' => env('AWS_REGION'),
+    'region' => env('AWS_DEFAULT_REGION'),
     'version' => 'latest',
     'ua_append' => [
         'L5MOD/' . AwsServiceProvider::VERSION,
