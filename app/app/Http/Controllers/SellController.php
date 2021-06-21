8b45fc6e7b2e25e4497e5c60fadf82f59ca919e3
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-06-21 09:59:21 +0530

    d

--- app/app/Http/Controllers/SellController.php
+++ app/app/Http/Controllers/SellController.php
@@ -128,7 +128,7 @@ class SellController extends Controller
         $request->session()->forget('sell_data');
         
         $user = Auth::user();
-        $Message = "[Route-Thai] P2P Order (Ending with " . substr($trans_id, -4) + ") of " . $data['quantity'] . " " . $selected_currency->name . " has been Successfully created. You will be notified when buyer gets matched.";
+        $Message = "[Route-Thai] P2P Order (Ending with " . substr($trans_id, -4) . ") of " . $data['quantity'] . " " . $selected_currency->name . " has been Successfully created. You will be notified when buyer gets matched.";
         Notify::sendMessage([
             'sms_notification' => $user->sms_notification,
             'mobile' => "mobile",
