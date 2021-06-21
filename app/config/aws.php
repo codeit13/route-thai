9ef99a32f1c8a19afcd34f9eba0de6ebeecf6342
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-06-21 21:39:05 +0530

    d

--- app/config/aws.php
+++ app/config/aws.php
@@ -17,8 +17,8 @@ return [
     |
     */
     'credentials' => [
-        'key'    => env('AWS_ACCESS_KEY_ID', ''),
-        'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
+        'key'    => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
     ],
     'region' => env('AWS_REGION'),
     'version' => 'latest',
