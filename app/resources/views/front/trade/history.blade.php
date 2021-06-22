a1e62deb354a5dde52b1fab77566f6c331428447
Niraj <nmsavaliya94@hotmail.com>
2021-06-22 11:48:47 +0530

    done

--- app/resources/views/front/trade/history.blade.php
+++ app/resources/views/front/trade/history.blade.php
@@ -241,7 +241,7 @@ Route: P2P Trading Platform
                                                 @if($transaction->type == 'buy')
                                                     @if($transaction->status == 'approved')
                                                         {{ $transaction->buyer_trans->user->name }}
-                                                    @elseif($transaction->status != 'approved' and $transaction->buyer_requests->count()>0)
+                                                    @elseif($transaction->status != 'approved' and $transaction->buyer_trans->buyer_requests->count()>0)
                                                         {{ $transaction->buyer_requests->first()->user->name }}
                                                     @else
                                                         NA
