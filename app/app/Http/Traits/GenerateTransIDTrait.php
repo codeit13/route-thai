86898d1aca8931b3bb792a2e0a60257f403e501a
Niraj <nmsavaliya94@hotmail.com>
2021-06-25 00:39:12 +0530

    done

--- app/app/Http/Traits/GenerateTransIDTrait.php
+++ app/app/Http/Traits/GenerateTransIDTrait.php
@@ -6,10 +6,21 @@ use Illuminate\Support\Facades\Storage;
 use App\Models\Transaction;
 
 trait GenerateTransIDTrait {
+	function random_string($length) {
+	    $key = '';
+	    $keys = array_merge(range(0, 9), range('a', 'z'));
+
+	    for ($i = 0; $i < $length; $i++) {
+	        $key .= $keys[array_rand($keys)];
+	    }
+
+	    return strtoupper($key);
+	}
+
 	public function generateID(){
 		$refrence_id = '';
 		do {
-		   $refrence_id = generate_unique_id();
+		   $refrence_id = random_string(8);
 		} while ( Transaction::where('trans_id', $refrence_id )->exists() );
 
 		return $refrence_id;
