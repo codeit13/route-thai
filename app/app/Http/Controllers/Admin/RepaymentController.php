f6038a607f909635193829f4bd26ed52bde07847
RandeepNamdhari <[randeepnamdhari87@gmail.com]>
2021-06-29 22:19:01 +0530

    loan system with admin work

--- app/app/Http/Controllers/Admin/RepaymentController.php
+++ app/app/Http/Controllers/Admin/RepaymentController.php
@@ -68,14 +68,27 @@ class RepaymentController extends Controller
                       case 'pending':
 
                       $action.='
-                       <a class="btn btn-sm btn-icon-only text-info d-inline-flex w-25" href="'.route('admin.loan.show',['loan'=>$loan->loan_request->loan_id]).'">
+                       <a data-toggle="tooltip" title="Details" class="btn btn-sm btn-icon-only text-info d-inline-flex w-25" href="'.route('admin.loan.show',['loan'=>$loan->loan_request->loan_id]).'">
                         <i class="fa fa-info m-auto"></i>
-                      </a>
-                      <a class="btn btn-sm btn-icon-only text-success w-25 d-inline-flex"  href="'.route('admin.loan.update.status',['id'=>$loan->id, 'status'=>'approved']).'">
-                        <i class="fa fa-check m-auto"></i>
-                      </a>
+                      </a>';
 
-                      <a class="btn btn-sm btn-icon-only text-danger d-inline-flex w-25" href="'.route('admin.loan.update.status',['id'=>$loan->id, 'status'=>'rejected']).'">
+                      if($loan->collateral_method!='wallet')
+                      {
+                        $action.='<a onclick="return confirm_action(this);" data-toggle="tooltip" title="Approve" class="btn btn-sm btn-icon-only text-success w-25 d-inline-flex"  href="'.route('admin.repayment.update.status',['id'=>$loan->id, 'status'=>'approved']).'">
+                        <i class="fa fa-check m-auto"></i></a>';
+                      }
+                      else
+                      {
+                        $action.='<a data-toggle="tooltip" title="Approve"  class="btn btn-sm btn-icon-only text-success w-25 d-inline-flex"  href="'.route('admin.repayment.update.status',['id'=>$loan->id, 'status'=>'approved']).'">
+                        <i class="fa fa-check m-auto"></i>
+                      </a>';
+
+                      }
+
+
+                  
+
+                     $action.='<a data-toggle="tooltip" title="Reject" class="btn btn-sm btn-icon-only text-danger d-inline-flex w-25" href="'.route('admin.repayment.update.status',['id'=>$loan->id, 'status'=>'rejected']).'">
                         <i class="fa fa-times m-auto"></i>
                       </a>
                        '; 
@@ -190,6 +203,109 @@ class RepaymentController extends Controller
             ->removeColumn('password')
             ->rawColumns(['collateral_info','loan_amount','user','action','detail_link','loan_repayment_amount'])
             ->make(true);
+    }
+
+
+    public function updateStatus($id, $status){
+
+        //echo $status;die;
+        
+            $repayment = LoanRepayRequest::find($id);
+            $repayment->status = trim($status);
+
+            $user=$repayment->user;
+            
+
+         if($status=='approved')
+        {
+
+            if($repayment->on_wallet)
+            {
+              
+              $wallet=$user->wallet()->where('wallet_type',1)->where('currency_id',$repayment->loan_currency_id)->first();
+
+              if(isset($wallet->coin) && $wallet->coin > $repayment->loan_repayment_amount)
+              {
+                 $wallet->coin=$wallet->coin-$repayment->loan_repayment_amount;
+
+                 $wallet->save();
+              }
+              else
+              {
+                return redirect()->back()->with('warning','User have not sufficent wallet balance to repay this loan.');
+              }
+
+
+
+            }
+
+        if($repayment->collateral_method=='wallet')
+        {
+
+             $wallet=$user->wallet()->where('wallet_type',1)->where('currency_id',$repayment->loan_request->currency_id)->first();
+
+        
+            if($wallet)
+            {
+                $wallet->coin=$wallet->coin+$repayment->loan_request->collateral_amount;
+                $wallet->save();
+            }
+            else
+            {
+                $newWallet=array('currency_id'=>$repayment->loan_request->currency_id,
+                                 'coin'=>$repayment->loan_request->collateral_amount,
+                                 'wallet_type'=>1,
+                                 '');
+                $user->wallet()->create($newWallet);
+            }
+
+
+
+        }
+
+           
+
+            
+
+            $repayment->status='approved';
+
+            $repayment->save();
+
+            $repayment->loan_request->status='repaid';
+
+            $repayment->loan_request->save();
+
+
+
+           
+
+           $notification='[Route-Thai] Your Loan order (Ending with '.substr($repayment->loan_request->loan_id,-4).' of '.$repayment->loan_request->loan_repayment_amount.' '.$repayment->loan_request->loan_currency->short_name.' has been successfully repaid.) ';
+
+        }
+        elseif ($status=='rejected') {
+
+            $repayment->status='rejected';
+
+            $repayment->save();
+
+          $notification='[Route-Thai] Your Loan order (Ending with '.substr($repayment->loan_request->loan_id,-4).' of '.$repayment->loan_request->loan_repayment_amount.' '.$repayment->loan_request->loan_currency->short_name.' has been rejected. Please contact support for more details.) ';
+        }
+
+          Notify::sendMessage([
+            'sms_notification' => $user->sms_notification,
+            'mobile' => $user->mobile,
+            'telegram_notification' => $user->telegram_notification,
+            'telegram_user_id' => $user->telegram_user_id,
+            'line_notification' => $user->line_notification,
+            'line_user_id' => $user->line_user_id,
+            'email_notification' => $user->email_notification,
+            'email_id' => $user->email,
+            'Message' => $notification,
+        ]);
+
+        return redirect()->back()->with('success','Loan and repayment status is updated successfully.');
+
+
     }
 
 
